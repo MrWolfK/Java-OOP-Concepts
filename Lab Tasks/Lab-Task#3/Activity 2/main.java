dac2fbece5610a9523172c351f7b9f142f654c2b
Malik M. Ahmed Awan <61834937+MrWolfK@users.noreply.github.com>
2026-07-07 22:34:41 +0500

    Added main class to calculate rectangle properties

---
+++ Lab Tasks/Lab-Task#3/Activity 2/main.java
@@ -0,0 +1,12 @@
+public class main 
+{
+public static void main(String[] args)
+{
+    Rectangle R = new Rectangle();
+    R.setLength(53.3);
+    R.setWidth(21.6);
+    System.out.println("Area of Rectangle isss: " + R.area());
+    System.out.println("Width of Rectangle is: " + R.getWidth());
+    System.out.println("Length of Rectangle is: "+ R.getLength());
+  }
+}

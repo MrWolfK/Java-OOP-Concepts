0b30aaa76e13df1def8fc87798390c4eb0610be2
Malik M. Ahmed Awan <61834937+MrWolfK@users.noreply.github.com>
2026-07-07 22:36:14 +0500

    Create main.java

---
+++ Lab Tasks/Lab-Task#3/Activity 3/main.java
@@ -0,0 +1,12 @@
+public class main {
+public static void main(String[] args)
+{
+Point p1=new Point();
+Point p2=new Point(2,3);
+p1.Xset(10);
+p1.Yset(15);
+p1.display();
+p2.movePoint(2,3);
+p2.display();
+}
+}

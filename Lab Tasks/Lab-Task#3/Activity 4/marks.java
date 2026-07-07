b88530e04b20698c8e8e92688bb80724cf9a1e2d
Malik M. Ahmed Awan <61834937+MrWolfK@users.noreply.github.com>
2026-07-07 22:38:36 +0500

    Created Marks class with encapsulation for marks
    Implemented a Marks class with encapsulation for three subject marks.

---
+++ Lab Tasks/Lab-Task#3/Activity 4/marks.java
@@ -0,0 +1,35 @@
+// Marks class demonstrating encapsulation for storing three subject marks.
+public class marks
+{
+private int marks1, marks2, marks3;
+  
+      public void setMarks1(int marks1)
+      {
+      this.marks1 = marks1;
+      }
+  
+      public void setMarks2(int marks2)
+      {
+      this.marks2 = marks2;
+      }
+  
+      public void setMarks3(int marks3)
+      {
+      this.marks3 = marks3;
+      }
+  
+      public int getMarks1()
+      {
+      return marks1;
+      }
+  
+      public int getMarks2()
+      {
+      return marks2;
+      }
+  
+      public int getMarks3()
+      {
+      return marks3;
+      }
+}

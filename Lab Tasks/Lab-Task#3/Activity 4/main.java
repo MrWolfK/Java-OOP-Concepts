bf697956cf511492cc0f1a7cb81068b5cd138775
Malik M. Ahmed Awan <61834937+MrWolfK@users.noreply.github.com>
2026-07-07 22:39:15 +0500

    Implemented main class to test marks setters and getters

---
+++ Lab Tasks/Lab-Task#3/Activity 4/main.java
@@ -0,0 +1,13 @@
+// Main class to testing the marks setters and getters.
+public class main {
+public static void main(String[] args)
+{
+      marks s = new marks();
+      s.setMarks1(67);
+      s.setMarks2(85);
+      s.setMarks3(89);
+      System.out.println("Marks 1 are: " + s.getMarks1());
+      System.out.println("Marks 2 are: " + s.getMarks2());
+      System.out.println("Marks 3 are: " + s.getMarks3());
+      }
+}
